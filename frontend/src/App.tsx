3689a0e40c894244ddf38d0cc7c8a4ff3b1cc8b1
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-09-06 22:49:52 +0400

    Change dashboard route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
 
         {/* <Route path="/home" element={<Home />} /> */}
       </Routes>
