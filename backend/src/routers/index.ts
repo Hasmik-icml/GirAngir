443abf2b1cfa74edf93dc7fc845ea2e4c2ac0311
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-09-10 13:15:32 +0400

    Create route for languages

--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { authRouter } from "./auth.routes";
+import { languageRouter } from "./language.router";
 
 const router: Router = Router();
 
 router.use('/auth', authRouter);
+router.use('/languages', languageRouter);
 
 export { router as router };
