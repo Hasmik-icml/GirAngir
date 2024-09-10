8bce33151500cf742461554080f03983e06b5629
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-09-10 13:14:02 +0400

    Add not found handler

---
+++ backend/src/handlers/not-found.handler.ts
@@ -0,0 +1,21 @@
+import { ValidationError } from "express-validator";
+import { CustomError } from "../errors/custom.error";
+
+export class NotFoundError extends CustomError {
+  statusCode = 404;
+
+  constructor(public errors: ValidationError[] | string) {
+    super(typeof errors === 'string' ? errors : 'Invalid request parameters');
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  }
+
+  serializeErrors() {
+    if (typeof this.errors === 'string') {
+      return [{ message: this.errors }];
+    } else {
+      return [{
+        message: this.errors[0].msg,
+      }];
+    }
+  }
+}
