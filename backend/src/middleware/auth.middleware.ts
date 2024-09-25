fa50c2e2ec1726295e6eee3802e1c9f4345243c3
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-09-25 08:53:18 +0400

    Improve auth middleware

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -13,7 +13,13 @@ export function authMiddleware(req: Request, res: Response, next: NextFunction) 
     const token = authHeader.split(' ')[1];
     try {
         const payload = jwt.verify(token, SECRET_KEY) as JwtPayload;
-        (req as any).userId = payload.userId;
+        const userId = Array.isArray(payload.userId) ? payload.userId[0] : payload.userId;
+
+        if (!userId || typeof userId !== 'string') {
+            return res.status(400).send({ message: 'Invalid or missing User ID' });
+        }
+
+        (req as any).userId = userId;
         next();
     } catch (error) {
         return res.status(401).send({ message: 'Unauthorized' });
