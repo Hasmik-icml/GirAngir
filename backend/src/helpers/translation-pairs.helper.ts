36d47ba723f5307799fae0fd5cd8c248bcd914c1
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-10-01 21:04:30 +0400

    Add pairsToRemove and pairsToCreate function helpers

--- backend/src/helpers/translation-pairs.helper.ts
+++ backend/src/helpers/translation-pairs.helper.ts
@@ -1,12 +1,33 @@
 
-export default function translationPairs(contentsIds: number[]): [number, number][] {
-    const translationPairs: [number, number][] = [];
+export class TranslationPairs {
 
-    for (let i = 0; i < contentsIds.length; i++) {
-        for (let j = i + 1; j < contentsIds.length; j++) {
-            translationPairs.push([contentsIds[i], contentsIds[j]]);
+    public static newPairs(contentsIds: string[]): string[][] {
+        const newPairs: string[][] = [];
+
+        for (let i = 0; i < contentsIds.length; i++) {
+            for (let j = i + 1; j < contentsIds.length; j++) {
+                newPairs.push([contentsIds[i], contentsIds[j]]);
+            }
         }
+
+        return newPairs;
     }
 
-    return translationPairs;
+    public static pairsToRemove(newPairs: string[][], existingPairs: string[][]): string[][] {
+        const pairsToRemove = existingPairs.filter(existingPair => {
+           return !newPairs.some(pair => (pair[0] === existingPair[1] && pair[1] === existingPair[2]) || (pair[0] === existingPair[2] && pair[1] === existingPair[1]));
+        })
+        return pairsToRemove;
+
+    }
+
+    public static pairsToCreate(newPairs: string[][], existingPairs: string[][]): string[][] {
+        const pairsToCreate = newPairs.filter(newPairs => {
+            return !existingPairs.some(pair => (pair[0] === newPairs[0] && pair[1] === newPairs[1]) || (pair[0] === newPairs[1] && pair[1] === newPairs[0]))
+        });
+
+        return pairsToCreate;
+    }
 }
+
+
