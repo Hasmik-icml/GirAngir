e5f5d149c036b12284ee86717c9335d8db1fee71
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-10-04 00:52:07 +0400

    Change button component

--- frontend/src/components/buttons/Button.tsx
+++ frontend/src/components/buttons/Button.tsx
@@ -3,11 +3,17 @@ import '../styles.css';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
+    size?: 'small' | 'medium' | 'large';
 }
 
-export default function Button({ children, ...props }: ButtonProps){
+export default function Button({ children, size = 'large', ...props }: ButtonProps){
+    const sizeClasses = {
+        small: 'py-2 px-3 text-xm w-auto',
+        medium: 'py-3 px-4 text-sm w-auto',
+        large: '',
+    };
     return (
-        <button className="button" {...props}>
+        <button className={`button ${sizeClasses[size]}`} {...props}>
             {children}
         </button>
     );
