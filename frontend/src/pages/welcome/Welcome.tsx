a2d5ada0123c2820f90bd13ffe55133053cf1f37
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-10-05 10:39:00 +0400

    Change logo size in welcome page

--- frontend/src/pages/welcome/Welcome.tsx
+++ frontend/src/pages/welcome/Welcome.tsx
@@ -9,7 +9,7 @@ export default function Welcome() {
                 <Button size="medium"><Link to="/register">Register</Link></Button>
             </div>
             <div className="flex flex-col md:flex-row items-center justify-center min-h-screen bg-gray-900">
-                <img src="/girangir.png" alt="Logo" className="mb-4" style={{ width: '20vw', height: '20vw' }} />
+                <img src="/girangir.png" alt="Logo" className="mb-4" style={{ width: '15vw', height: '15vw' }} />
                 <div className="flex flex-col md:flex-row items-center text-center">
                     <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4 animate-bounce text-gray-500">Welcome</h1>
                     <p className="text-lg md:text-xl text-gray-400">GirAngir</p>
