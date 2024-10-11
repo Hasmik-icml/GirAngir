63e19f30c96cf9fda47cbf0e6fb8adec98d4b638
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-10-11 10:56:44 +0400

    Fix the bug with the responsivness

--- frontend/src/pages/home/Dashboard.tsx
+++ frontend/src/pages/home/Dashboard.tsx
@@ -4,12 +4,14 @@ import Vocabulary from "../../components/dashboard/Vocabulary";
 export default function Dashboard() {
     return (
         <>
+            <div className="flex flex-col sm:flex-row min-h-screen">
                 <div className="w-full sm:w-64 bg-gray-800 text-white sm:min-h-screen">
                     <Sidebar />
                 </div>
-                <div className="flex-1 bg-gray-100 p-4">
+                <div className="flex-1 bg-gray-100 p-4 overflow-auto">
                     <Vocabulary />
                 </div>
+            </div>
         </>
     )
 }
